007ff512689dbf1a1673b62e3682ee3de758b99a
Van Simmons <rvsrvs@gmail.com>
2019-04-17 22:46:22 -0400

    adding tester

--- echoserver/test.sh
+++ echoserver/test.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+echo "Running test of echo server"
+echo ""
+echo "Output:"
 curl -X "POST" "http://localhost:8080/echo" \
      -H 'Content-Type: text/plain; charset=utf-8' \
      -d $'{
   "sometag": "with some value",
   "someothertag": "and some other value"
 }'
+echo ""
+
